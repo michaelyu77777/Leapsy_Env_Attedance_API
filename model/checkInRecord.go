4c14d17b2dc3dba42bd41b947eb9a9ff87bdf8df
michaelyu77777 <69225666+michaelyu77777@users.noreply.github.com>
2021-07-20 18:18:05 +0800

    第一階段改版成功可跑

--- model/checkInRecord.go
+++ model/checkInRecord.go
@@ -1,12 +1,16 @@
 package model
 
+import "time"
+
 type CheckInRecord struct {
-	id            int    //`json:"_id"`
-	Name          string //注意:struct名稱開頭必須要大寫...否則無法寫入mongoDB!!!不知道為什麼...
-	Check_in_time string
-	Pic           string
-	Leave_type    string
-	Date          string
-	Department    string
-	Position      string
+	id              int       `json:"id"`
+	Name            string    `json:"name"` //注意:struct名稱開頭必須要大寫...否則無法寫入mongoDB!!!不知道為什麼...
+	Check_in_time   string    `json:"checkintime"`
+	Pic             string    `json:"pic"`
+	Leave_type      string    `json:"leavetype"`
+	Date            string    `json:"date"`
+	Department      string    `json:"department"`
+	Position        string    `json:"position"`
+	DateTimeToday   time.Time `json:"datetimetoday"`
+	DateTimeCheckIn time.Time `json:"datetimecheckin"`
 }
